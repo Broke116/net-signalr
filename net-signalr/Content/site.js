971092e77ad473fe7701161bfa2d18e9bd0f22d8
Broke116 <ekiny018@gmail.com>
2017-03-30 22:00:05 +0300

    json file update

--- net-signalr/Content/site.js
+++ net-signalr/Content/site.js
@@ -1,10 +1,10 @@
 ﻿$(document).ready(function () {
-    $('#myModal').on('show.bs.modal', function (e) {
-        var productId = $(e.relatedTarget).data('prod-id');
-        $.get('/product/' + productId, function (data) {
-            $("#productId").val(productId);
-            console.log(data);
-            $("#ProductName").val(data.ProductName);
-        });
-    });
+    //$('#myModal').on('show.bs.modal', function (e) {
+    //    var productId = $(e.relatedTarget).data('prod-id');
+    //    $.get('/product/' + productId, function (data) {
+    //        $("#productId").val(productId);
+    //        console.log(data);
+    //        $("#ProductName").val(data.ProductName);
+    //    });
+    //});
 });
--- net-signalr/Controllers/HomeController.cs
+++ net-signalr/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
 using net_signalr.Services;
 using net_signalr.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace net_signalr.Controllers
 {
@@ -27,6 +32,61 @@ namespace net_signalr.Controllers
             var product = service.Get(productId);
 
             return product;
+        }
+
+        [HttpGet]
+        public ActionResult GetProductDetail(int id)
+        {
+            var product = service.Get(id);
+
+            return PartialView("Partials/_EditProductPartial", product);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateProduct(Product model)
+        {
+            string output = "";
+            if (ModelState.IsValid)
+            {
+                //var updatedProduct = new Product
+                //{
+                //    ProductID = model.ProductID,
+                //    ProductName = model.ProductName,
+                //    Category = model.Category,
+                //    Price = model.Price,
+                //    Quantity = model.Quantity
+                //};
+
+                var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Models/products.json");
+                using (var r = new StreamReader(path))
+                {
+                    var json = r.ReadToEnd();
+                    var jsonObj = JsonConvert.DeserializeObject<List<Product>>(json);
+
+                    foreach(var val in jsonObj)
+                    {
+                        if(val.ProductID == model.ProductID)
+                        {
+                            val.ProductName = model.ProductName;
+                            val.Category = model.Category;
+                            val.Price = model.Price;
+                            val.Quantity = model.Quantity;
+                        }
+                    }
+
+                    output = JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+                    r.Close();
+                }
+                
+                using (var w = new StreamWriter(path))
+                {
+                    w.Write(output);
+                    w.Close();
+                }
+                //System.IO.File.WriteAllText(path, output);
+            }
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
