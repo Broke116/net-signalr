6b5db324da81beb81690b07a84b84fd93a8f39c0
Broke116 <ekiny018@gmail.com>
2017-04-08 16:34:54 +0300

    user count

--- net-signalr/Hubs/ProductHub.cs
+++ net-signalr/Hubs/ProductHub.cs
@@ -1,5 +1,7 @@
-﻿using DAL.Models;
+﻿using System.Threading.Tasks;
+using DAL.Models;
 using Microsoft.AspNet.SignalR;
+using net_signalr.Models;
 
 namespace net_signalr.Hubs
 {
@@ -8,6 +10,39 @@ namespace net_signalr.Hubs
         public ProductHub()
         {
 
+        }
+
+        public override async Task OnConnected()
+        {
+            var userId = Context.ConnectionId;
+            if(User.users.IndexOf(userId) == -1)
+            {
+                User.users.Add(userId);
+            }
+
+            await UserCount(User.users.Count, true);
+        }
+
+        public override async Task OnDisconnected(bool stopCalled)
+        {
+            var userId = Context.ConnectionId;
+            if (User.users.IndexOf(userId) > -1)
+            {
+                User.users.Remove(userId);
+            }
+
+            await UserCount(User.users.Count, false);
+        }
+
+        public async Task UserCount(int userCount, bool isConnect)
+        {
+            await Clients.All.showUserCount(userCount);
+        }
+
+        public static void Show(ProductViewModel product)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<ProductHub>();
+            context.Clients.All.displayChanges();
         }
 
         public void Update(Product product)
---
+++ net-signalr/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace net_signalr.Models
+{
+    public static class User
+    {
+        public static List<string> users = new List<string>();
+        public static object lockObject = new Object();
+    }
+}
